155ae412fa2adbae2c9bb3052eb8eb7163f39d33
mateuszstraszewski <strachoo@gmail.com>
2021-07-17 10:20:58 +0200

    Project init

---
+++ src/main/java/com/project/oskapi/OskApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.oskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/oskapi/OskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.oskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
